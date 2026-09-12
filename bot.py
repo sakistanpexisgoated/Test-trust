35f6aff002820fc7066e262f6103b3f7c99bb3d9
sakistanpexisgoated <faiqanees94@gmail.com>
2026-09-12 16:56:38 +0300

    Fix command name casing for trivia command

--- bot.py
+++ bot.py
@@ -4669,7 +4669,7 @@ async def start_country_setup(channel, player_id):
     
     await channel.send("Select difficulty below:", view=view)
 
-@bot.hybrid_command(name="Triva", description="Start a Flag Triva")
+@bot.hybrid_command(name="triva", description="Start a Flag Triva")
 async def country(ctx):
     await start_country_setup(ctx.channel, ctx.author.id)
 
